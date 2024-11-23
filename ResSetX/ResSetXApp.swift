7b05a7d4a85de47588d48ebb6f067304175af37e
Skadz <skadz@skadz.online>
2024-11-22 20:08:29 -0700

    Initial Commit

---
+++ ResSetX/ResSetXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResSetXApp.swift
+//  ResSetX
+//
+//  Created by Skadz on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ResSetXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
